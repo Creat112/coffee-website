ca579736bdeeee19c595148141b2f3797e609bf6
Creat112 <106399371+Creat112@users.noreply.github.com>
2025-10-24 16:31:26 +0300

    Update product data and display logic in main.js

--- main.js
+++ main.js
@@ -2,66 +2,31 @@ const container = document.getElementById("products-container");
 
 // البيانات كانت في data.json — نقلناها هنا مباشرة
 const data = [
+
   {
     "id": 1,
-    "name": "Espresso",
-    "price": 45,
-    "image": "espresso.png"
-  },
-  {
-    "id": 2,
-    "name": "Cappuccino",
-    "price": 55,
-    "image": "cappuccino.jpg"
-  },
-  {
-    "id": 3,
-    "name": "Latte",
-    "price": 60,
-    "image": "latte.png"
-  },
-  {
-    "id": 4,
     "name": "Mocha",
     "price": 65,
     "image": "mocha.png"
   },
   {
-    "id": 5,
+    "id": 2,
     "name": "Iced Coffee",
     "price": 50,
     "image": "iced-coffee.jpg"
   },
   {
-    "id": 6,
-    "name": "Cold Brew",
-    "price": 70,
-    "image": "cold-brew.jpg"
-  },
-  {
-    "id": 7,
+    "id": 3,
     "name": "Croissant",
     "price": 35,
     "image": "croissants.jpg"
   },
   {
-    "id": 8,
-    "name": "Blueberry Muffin",
-    "price": 40,
-    "image": "blueberry-muffin.jpg"
-  },
-  {
-    "id": 9,
+    "id": 4,
     "name": "Chocolate Cake",
     "price": 80,
     "image": "chocolate-cake.jpg"
   },
-  {
-    "id": 10,
-    "name": "Matcha Latte",
-    "price": 65,
-    "image": "matcha-latte.jpg"
-  }
 ];
 
 // نفس طريقة عرض الكروت
@@ -78,3 +43,4 @@ data.forEach(item => {
 
   container.appendChild(card);
 });
+
